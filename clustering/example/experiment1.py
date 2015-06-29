79739d316113732ad1eff9e66cb89a2120a305f5
ZAZAZAkari <zacharylai@yobi3d.com>
2015-06-29 17:12:47 +0800

    1. adding hint comments

--- clustering/example/experiment1.py
+++ clustering/example/experiment1.py
@@ -30,13 +30,15 @@ def main():
 	trainingData = np.loadtxt(TRAINING_SET_FILE_NAME)
 	testData = np.loadtxt(PREDICTION_SET_FILE_NAME)
 	
-	clusteredResult = clustering.train(trainingData, LEARNT_MODEL_NAME, True)
+	# PARAMS: TRAINING_DATA, LEARNT MODEL, CLUSTER ALL?, NUMBER OF CLUSTERS 
+	clusteredResult = clustering.train(trainingData, LEARNT_MODEL_NAME, True, 3)
 	
 	# ========== TAKE THE VALUE FROM clusteredResult AS YOU NEED ============ #
 	# print (clusteredResult['numberOfClusters'])
 	# print (clusteredResult['clusterCenters'])
 	# print (clusteredResult['labels'])
 	
+	# PARAMS: TRAINING_DATA, LEARNT MODEL
 	predictionResult = clustering.predict(testData, LEARNT_MODEL_NAME)
 	
 	#print predictionResult
